d18d518b7ec5f53cb18af4e61bfc2b24fea77fb9
Панов Антон <A.Panov@expobank.ru>
2019-08-09 15:34:50 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,9 @@ export default {
         return {
             sConfigurationFileName: '.appGitMarkdownDocs.json',
             sConfigurationFilePath: '',
-            oConfiguration: {}
+            sRepositoriesDirName: '.appGitMarkdownDocs_repositories',
+            sRepositoriesDirPath: '',
+            oConfiguration: null
         }
     },
     
@@ -34,7 +36,7 @@ export default {
         
         Notify.setDefaults({
             position: 'top-right',
-            timeout: 2500,
+            timeout: 100000,
             textColor: 'white',
             actions: [{ icon: 'close', color: 'white' }]
         })
@@ -50,9 +52,38 @@ export default {
             this.$q.notify({
                 color: 'negative', 
                 message: `The settings file ('${this.sConfigurationFilePath}') was not found and a new one will be created. The default settings will be used.`, 
-                icon: 'report_problem',
-                timeout: 100000
+                icon: 'report_problem'
             });
+        }
+        
+        if (!this.oConfiguration) {
+            this.oConfiguration = {};
+        }
+        
+        this.sRepositoriesDirName = this.oConfiguration['sRepositoriesDirName'] ? this.oConfiguration['sRepositoriesDirName'] : this.sRepositoriesDirName;
+        this.sRepositoriesDirPath = this.oConfiguration['sRepositoriesDirPath'] ? this.oConfiguration['sRepositoriesDirPath'] : path.join(os.homedir(), this.sRepositoriesDirName);
+        
+        console.log('Repositories dir path', this.sRepositoriesDirPath, fs.existsSync(this.sRepositoriesDirPath));
+        
+        if (!fs.existsSync(this.sRepositoriesDirPath)) {
+            console.log('Repositories dir not found', this.sRepositoriesDirPath);
+            try {
+                fs.mkdirSync(this.sRepositoriesDirPath, { recursive: true });
+            } catch(oException) {
+                console.log('Repositories dir couldn\'t be created', this.sRepositoriesDirPath);
+                this.$q.notify({
+                    color: 'negative', 
+                    message: `It is not possible to create a directory ('${this.sRepositoriesDirPath}')`, 
+                    icon: 'report_problem'
+                });
+            } finally {
+                console.log('Repositories dir created', this.sRepositoriesDirPath);
+                this.$q.notify({
+                    color: 'positive', 
+                    message: `Created directory for repositories ('${this.sRepositoriesDirPath}')`, 
+                    icon: 'check'
+                });
+            }
         }
     }
 }
